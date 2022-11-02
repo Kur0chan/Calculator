4b20bb2fc3007846f0914cc6dc68f4a5e3c9fffd
Андрей <excellentman221@gmail.com>
2022-11-02 23:26:47 +0400

    3

---
+++ src/Numbers.java
@@ -0,0 +1,5 @@
+public class Numbers {
+    int numberOne;
+    int numberTwo;
+}
+
---
+++ src/Operators.java
@@ -0,0 +1,28 @@
+public class Operators {
+    private String numberWithNumber;
+    private String operator;
+
+    public Operators(String numberWithNumber, String operator) {
+        this.numberWithNumber = numberWithNumber;
+        this.operator = operator;
+    }
+
+    public String getNumberWithNumber() {
+        return numberWithNumber;
+    }
+
+    public void setNumberWithNumber(String numberWithNumber) {
+        this.numberWithNumber = numberWithNumber;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+}
+
+
+
